0db62f84d73a4e38ec601bd99756204185078d0d
jw00ds <woods.johnc@yahoo.com>
2020-11-27 15:06:21 -0600

    refactor: update navbar

---
+++ src/common/Nav.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Nav: React.FC = () => {
+  return (
+    <nav className="navbar navbar--dark">
+      <div className="navbar__inner">
+        <div className="navbar__items">
+          <NavLink className="navbar__item navbar__link" exact to="/">
+            Home
+          </NavLink>
+          {/* <NavLink className="navbar__item navbar__link" exact to="/register">
+          Register
+          </NavLink> */}
+          {/* <NavLink className="navbar__item navbar__link" exact to="/login">
+          Login
+          </NavLink> */}
+          <NavLink
+            className="navbar__item navbar__link"
+            exact
+            to="/testfoodinput"
+          >
+            Test Food Input
+          </NavLink>
+        </div>
+        <div className="navbar__items navbar__items--right">
+          <form>
+            <div className="navbar__search">
+              <input className="navbar__search-input" placeholder="Search" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
