25468ab57574699d2dc7e10c6e880b336dd4db11
aTmb405 <aTmb405@gmail.com>
2020-12-07 21:32:12 -0600

    refactor: fix eslint errors

--- src/app/Routes.tsx
+++ src/app/Routes.tsx
@@ -1,38 +1,36 @@
-import React, { Suspense } from "react";
-import { Switch, Route, RouteProps, Redirect } from "react-router-dom";
-import Home from "../common/Home";
-import TestIpts from "../feats/TestIpts";
-import ImageReader from "../feats/ImageReader";
-import PrivateRoute from "../common/PrivateRoute";
-import Register from "../common/Register";
-import Login from "../common/Login";
+import React, { Suspense } from 'react';
+import { Switch, Route, RouteProps, Redirect } from 'react-router-dom';
+import Home from '../common/Home';
+import TestIpts from '../feats/TestIpts';
+import ImageReader from '../feats/ImageReader';
+import PrivateRoute from '../common/PrivateRoute';
+import Register from '../common/Register';
+import Login from '../common/Login';
 
 // Remember to protect new fuctionality with PrivateRoute
-const Routes: React.FC<RouteProps> = () => {
-  return (
-    <div>
-      <Suspense fallback={<div></div>}>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <PrivateRoute exact path="/testfoodinput">
-            <TestIpts />
-          </PrivateRoute>
-          <Route exact path="/imageInput">
-            <ImageReader />
-          </Route>
-          <Route exact path="/register">
-            <Register />
-          </Route>
-          <Route exact path="/login">
-            <Login />
-          </Route>
-          <Redirect from="*" to="/" />
-        </Switch>
-      </Suspense>
-    </div>
-  );
-};
+const Routes: React.FC<RouteProps> = () => (
+  <div>
+    <Suspense fallback={<div />}>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <PrivateRoute exact path="/testfoodinput">
+          <TestIpts />
+        </PrivateRoute>
+        <Route exact path="/imageInput">
+          <ImageReader />
+        </Route>
+        <Route exact path="/register">
+          <Register />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Redirect from="*" to="/" />
+      </Switch>
+    </Suspense>
+  </div>
+);
 
 export default Routes;
