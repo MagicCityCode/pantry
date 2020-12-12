a7038f536e567f11160c91e236c976ead4b02ab9
jw00ds <woods.johnc@yahoo.com>
2020-12-12 16:39:03 -0600

    refactor: separate recipe search and ingredient input from test component

---
+++ src/feats/IngredientInput.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { useHistory } from 'react-router';
+import service from '../utils/api-service';
+
+export default function TestInputs() {
+  const history = useHistory();
+  const [newFormEntry, setNewFormEntry] = React.useState<{ [key: string]: string }>({});
+  const handleFormChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.persist();
+    setNewFormEntry((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const handleFormSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    service.handleFetch('/ingredients', 'POST', newFormEntry);
+    history.push('/');
+  };
+
+  return (
+    <div className="row row--align-center row--justify-center">
+      <div className="card-demo margin-vert--md">
+        <div className="card">
+          <div className="col col--12">
+            <h3 className="card--title">Submit New Ingredient</h3>
+            <div className="card__body">
+              <input
+                className="form-input margin-bottom--md"
+                value={newFormEntry.name || ''}
+                type="text"
+                name="name"
+                placeholder="Name"
+                onChange={handleFormChange}
+              />
+              <input
+                className="form-input margin-bottom--md"
+                value={newFormEntry.shelfLife || ''}
+                type="text"
+                name="shelfLife"
+                placeholder="Shelf life"
+                onChange={handleFormChange}
+              />
+              <input
+                className="form-input margin-bottom--md"
+                value={newFormEntry.shelfLifeUnit || ''}
+                type="text"
+                name="shelfLifeUnit"
+                placeholder="Shelf life unit (e.g. days, weeks, years)"
+                onChange={handleFormChange}
+              />
+              <input
+                className="form-input margin-bottom--md"
+                value={newFormEntry.storageMethod || ''}
+                type="text"
+                name="storageMethod"
+                placeholder="Storage method/location"
+                onChange={handleFormChange}
+              />
+              <input
+                className="form-input margin-bottom--md"
+                value={newFormEntry.unitOfMeasure || ''}
+                type="text"
+                name="unitOfMeasure"
+                placeholder="Unit of measure"
+                onChange={handleFormChange}
+              />
+              <button
+                type="button"
+                className="button button--outline button--primary"
+                onClick={handleFormSubmit}
+              >
+                Submit New Ingredient
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/feats/RecipeSearch.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import service from '../utils/api-service';
+
+export default function RecipeSearch() {
+  const [newFormEntry, setNewFormEntry] = React.useState<{ [key: string]: string }>({});
+  const handleFormChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.persist();
+    setNewFormEntry((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const handleFormSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    service.handleFetch('/recipes-by-ingredients', 'POST', newFormEntry);
+  };
+
+  return (
+    <div className="row row--align-center row--justify-center">
+      <div className="card-demo margin-vert--md">
+        <div className="card">
+          <div className="col col--12">
+            <h3 className="card--title">Search Recipes by Ingredients</h3>
+            <div className="card__body">
+              <input
+                className="form-input margin-bottom--md"
+                value={newFormEntry.listOfIngredients || ''}
+                type="text"
+                name="listOfIngredients"
+                placeholder="List ingredients with commas"
+                onChange={handleFormChange}
+              />
+              <button
+                type="button"
+                className="button button--outline button--primary"
+                onClick={handleFormSubmit}
+              >
+                Submit Search
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
