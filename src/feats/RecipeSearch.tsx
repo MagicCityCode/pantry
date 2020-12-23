58286dc6bb9d1c51bc2938013b3b5e71693d95ed
jw00ds <woods.johnc@yahoo.com>
2020-12-22 18:24:23 -0600

    feat: initial display of recipe results from inventory

--- src/feats/RecipeSearch.tsx
+++ src/feats/RecipeSearch.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-// import service from '../utils/api-service';
+// import service from '../utils/api-service'; // Bring this back soon
 import { ISpoonacularRecipe } from '../utils/types';
 
 const RecipeSearch: React.FC = () => {
@@ -43,26 +43,36 @@ const RecipeSearch: React.FC = () => {
       .then((r) => {
         setUserAvailableInventory(r);
       })
-      .catch((err) => console.log({ Error: err, Status: err.status, Message: err.message }));
+      .catch((err) => console.log(err));
   }, []);
-  let ingredientsFromInventoryString = '';
-  // Soon add duplicate-omission to for loop below
+
   const prepIngredientsListForSpoonacular = (arr: any) => {
-    if (userAvailableInventory.length >= 1) {
-      for (let i = 0; i < userAvailableInventory.length; i + 1) {
-        if (userAvailableInventory.length === 1) {
-          ingredientsFromInventoryString = arr[i].item;
-        } else if (i === userAvailableInventory.length - 1) {
-          ingredientsFromInventoryString.concat(String(`${arr[i].item}`));
-        }
-        ingredientsFromInventoryString.concat(String(`${arr[i].item}, `));
-      }
-      return ingredientsFromInventoryString;
-    }
-    return 'durian';
+    // function prepIngredientsListForSpoonacular(arr) {
+    //   let ingredientsFromInventoryString = '';
+    //   if (arr.length === 1) {
+    //     ingredientsFromInventoryString = arr[0];
+    //   } else if (arr.length === 2) {
+    //     ingredientsFromInventoryString = arr[0] + ', ' + arr[1];
+    //   } else {
+    //     for (let i = 0; i < arr.length; i++) {
+    //       if (i === arr.length - 1) {
+    //         ingredientsFromInventoryString = ingredientsFromInventoryString + arr[i];
+    //       } else if (i === 0) {
+    //         ingredientsFromInventoryString = arr[i];
+    //       } else {
+    //         ingredientsFromInventoryString = ingredientsFromInventoryString + arr[i] + ', ';
+    //       }
+    //     }
+    //   }
+    //   return ingredientsFromInventoryString;
+    // }
+    // prepIngredientsListForSpoonacular(foodArr);
+    const ingredientsArr = arr.map((food: { item: any }) => food.item);
+    const ingredientsString = ingredientsArr.join(', ');
+    return ingredientsString;
   };
 
-  const handleFormSubmitByInventory = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const handleSubmitByInventory = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     const ingredients: string = prepIngredientsListForSpoonacular(userAvailableInventory);
     fetch('/recipes-by-ingredients', {
@@ -105,7 +115,7 @@ const RecipeSearch: React.FC = () => {
                 <button
                   type="button"
                   className="button button--outline button--primary"
-                  onClick={handleFormSubmitByInventory}
+                  onClick={(e) => handleSubmitByInventory(e)}
                 >
                   Submit Inventory Search
                 </button>
